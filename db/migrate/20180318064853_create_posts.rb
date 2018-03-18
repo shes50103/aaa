2857090e65eb65be03dc0f180800db65d9345a64
shes50103 <shes50103@gmail.com>
2018-03-18 14:50:58 +0800

    db:migrate

---
+++ db/migrate/20180318064853_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :state
+      t.text :content
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
